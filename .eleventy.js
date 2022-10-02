2720d2b89f6992aca6b24787c1be6bf8214f34ab
Dominic <dominic.kirste@gmail.com>
2022-10-02 15:59:39 +0200

    added favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy("src/meta");
+  eleventyConfig.addPassthroughCopy("src/favicon");
+  eleventyConfig.addPassthroughCopy("src/*.ico");
   eleventyConfig.addWatchTarget("postcss/**/*.css");
 
   return {
